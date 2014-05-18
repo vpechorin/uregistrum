6ad2cdb275ecd8bf52aa36b24a0b4f0cf2acedb0
Victor Pechorin <dev@pechorina.net>
2014-05-19 11:55:55 +1200

    Updated spring boot version, minor improvements

--- src/main/java/net/pechorina/uregistrum/web/EndpointController.java
+++ src/main/java/net/pechorina/uregistrum/web/EndpointController.java
@@ -2,6 +2,7 @@ package net.pechorina.uregistrum.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.pechorina.uregistrum.exceptions.EndpointExistsException;
@@ -10,12 +11,15 @@ import net.pechorina.uregistrum.model.Endpoint;
 import net.pechorina.uregistrum.service.EndpointService;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/endpoints")
 public class EndpointController {
+	private static final Logger logger = LoggerFactory.getLogger(EndpointController.class);
 
 	@Autowired
 	Environment env;
@@ -56,46 +61,42 @@ public class EndpointController {
 			@RequestParam(value = "name", required = true) String name,
 			@RequestParam(value = "address", required = true) String address,
 			@RequestParam(value = "description", required = false, defaultValue = "") String description,
-			@RequestParam(value = "version", required = false, defaultValue = "") String version) throws EndpointExistsException {
+			@RequestParam(value = "version", required = false, defaultValue = "") String version,
+			HttpServletRequest request) throws EndpointExistsException {
 		Endpoint savedEntity = endpointService.addEndpoint(name, address,
 				description, version);
+		logger.info("CREATE: " + name + " - " + address + " Src:" + request.getRemoteAddr());
 		EndpointResource resource = endpointResourceAssembler
 				.toResource(savedEntity);
 		return new ResponseEntity<EndpointResource>(resource,
 				HttpStatus.CREATED);
 	}
 
-	@RequestMapping(method = RequestMethod.PUT)
-	public ResponseEntity<EndpointResource> saveOrUpdateEndpoint(
-			@RequestParam(value = "name", required = true) String name,
-			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "description", required = false) String description,
-			@RequestParam(value = "version", required = false) String version) {
+	@RequestMapping(method = RequestMethod.PUT, value = "/{name}")
+	public void saveOrUpdateEndpoint(@PathVariable("name") String name, @RequestBody Endpoint endpoint,
+			HttpServletRequest request,
+			HttpServletResponse response)	{		
+		
 		Endpoint e;
 		try {
 			e = endpointService.lookupEndpoint(name);
+			endpoint.setRegistered(e.getRegistered());
 		} catch (EndpointNotFoundException e1) {
-			e = new Endpoint();
-			e.setRegistered(new DateTime());
-			e.setName(name);
+			e = endpoint;
+			endpoint.setRegistered(new DateTime());
+			endpoint.setName(name);
 		}
-		if (address != null)
-			e.setAddress(address);
-		if (description != null)
-			e.setDescription(description);
-		if (version != null)
-			e.setVersion(version);
-		Endpoint savedEntity = endpointService.saveEndpoint(e);
-		EndpointResource resource = endpointResourceAssembler
-				.toResource(savedEntity);
-		return new ResponseEntity<EndpointResource>(resource,
-				HttpStatus.CREATED);
+
+		endpointService.saveEndpoint(endpoint);
+		logger.info("UPDATE: " + endpoint + " Src:" + request.getRemoteAddr());
+		response.setStatus(HttpServletResponse.SC_OK);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/{name}")
 	public void remove(
-			@PathVariable("name") String name, HttpServletResponse response) throws EndpointNotFoundException {
+			@PathVariable("name") String name, HttpServletRequest request, HttpServletResponse response) throws EndpointNotFoundException {
 		endpointService.deleteEndpoint(name);
+		logger.info("DELETE: " + name + " Src:" + request.getRemoteAddr());
 		response.setStatus(HttpServletResponse.SC_OK);
 	}
 
